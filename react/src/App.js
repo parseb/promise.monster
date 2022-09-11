912af4c5531109ca6ea2023111986f8252b36d76
parseb <petra306@protonmail.com>
2022-09-11 21:17:07 +0300

    adds walletconnect and color change

--- react/src/App.js
+++ react/src/App.js
@@ -13,17 +13,29 @@ function App() {
       <header className="App-header"> 
       </header>
       <div className='container main-container'>
+        <div class="logo-landing">
         <div className='row'>
         <div className='col-3'>
           </div>
           <div className='col-6'>
-            <h2 className='logo-text text-center'>promise 👾 monster</h2>
+            <h1 className='text-center monster-text'>👾</h1>
+          </div>
+          <div className='row'>
+          <div className='col-3'>
+          </div>
+        </div>
+        <div className='col-3'>
+          </div>
+          <div className='col-6'>
+            <h2 className='logo-text text-center'>promise monster</h2>
           </div>
           <div className='col-3'>
           </div>
         </div>
+        </div>
+        </div>
       </div>
-    </div>
+
   );
 }
 
